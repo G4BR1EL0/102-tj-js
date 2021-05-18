887ee870ce1789c36dd37fbc5ac2641de1e449b6
G4BR1EL0 <gabrielabarca.91@gmail.com>
2021-05-18 12:43:39 +0200

    15 minutos en realizar 2 intentos

--- test/test01.js
+++ test/test01.js
@@ -1,4 +1,16 @@
 function clasificaElementos(index) {
-    return [0,0,0];
+    let numerosPositivos = 0;
+    let numerosNegativos = 0;
+    let numerosCero = 0;
+    index.forEach(num => {
+        if (num>0) numerosPositivos++;
+        if (num<0) numerosNegativos++;
+        if (num === 0) numerosCero++;
+    });
+    let cantidad = index.length
+    let positivos = (numerosPositivos/cantidad).toFixed(4);
+    let negativos = (numerosNegativos/cantidad).toFixed(4);
+    let ceros = (numerosCero/cantidad).toFixed(4);
+    return [positivos,negativos,ceros];
 }
 module.exports = clasificaElementos;
